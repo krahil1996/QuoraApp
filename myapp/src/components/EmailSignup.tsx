48685ff93b2ccd1d26e9fa78f1bdee7a35723847
krahil1996 <rahil393@gmail.com>
2024-09-18 18:19:22 +0530

    emailsignupSuccess

--- myapp/src/components/EmailSignup.tsx
+++ myapp/src/components/EmailSignup.tsx
@@ -1,33 +1,35 @@
-import {
-  createUserWithEmailAndPassword,
-  onAuthStateChanged,
-  sendEmailVerification,
-} from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/Setup";
 import { useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 type emailType = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   setEmailSignup: any;
 };
 const EmailSignup = (props: emailType) => {
+  const [name, SetName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const Signup = async () => {
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      toast.success("Please verify the mail Id");
-      onAuthStateChanged(auth, (user: any) => {
-        sendEmailVerification(user);
-      });
-    } catch (err) {
-      console.error(err);
+      await createUserWithEmailAndPassword(auth, email, password).then(
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        async (result: any) => {
+          result.user.displayName = name;
+          const Uname = result.user.displayName;
+          const email = result.user.email;
+          const accessToken = result.user.accessToken;
+          console.log(Uname);
+          console.log(email);
+          console.log(accessToken);
+        }
+      );
+    } catch (FirebaseError) {
+      console.log(FirebaseError);
     }
   };
 
-  console.log(auth);
   return (
     <div>
       <div
@@ -46,8 +48,8 @@ const EmailSignup = (props: emailType) => {
             <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl">
               <div className=" flex justify-center items-center rounded-full mx-1 mt-2 w-5 h-5 p-5  hover:bg-gray-100">
                 <button
+                  className="focus:outline-transparent"
                   onClick={() => props?.setEmailSignup(false)}
-                  className=""
                 >
                   <svg
                     width="24"
@@ -73,20 +75,32 @@ const EmailSignup = (props: emailType) => {
                 </h1>
                 <h1 className="text-base font-semibold my-1">Name</h1>
                 <input
+                  onChange={(e) => SetName(e.target.value)}
                   className="p-2 w-full border outline-none rounded-ssm hover:border-blue-600 transition-all ease-in-out focus:border-blue-600 "
                   type="text"
                   placeholder="What would you like to be called?"
                 />
                 <h1 className="text-base font-semibold my-1">Email</h1>
                 <input
+                  onChange={(e) => setEmail(e.target.value)}
                   className="p-2 w-full border outline-none rounded-ssm hover:border-blue-600 transition-all ease-in-out focus:border-blue-600 "
                   type="email"
                   placeholder="Your Email"
                 />
+                <h1 className="text-base font-semibold my-1">Password</h1>
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="p-2 w-full border outline-none rounded-ssm hover:border-blue-600 transition-all ease-in-out focus:border-blue-600 "
+                  type="text"
+                  placeholder="Your Password"
+                />
               </div>
               <hr />
               <div className=" flex justify-end m-4 ">
-                <button className="   bg-blue-600 text-white w-20 h-10 rounded-full ">
+                <button
+                  onClick={Signup}
+                  className="   bg-blue-600 text-white w-20 h-10 rounded-full focus:outline-white "
+                >
                   Next
                 </button>
               </div>
