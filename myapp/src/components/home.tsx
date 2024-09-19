201bccf2fce5cf8da864dc2656ecd8022096c9cf
Mohammad Arwaz <mohdarwaz66@gmail.com>
2024-09-19 19:11:26 +0530

    main

--- myapp/src/components/home.tsx
+++ myapp/src/components/home.tsx
@@ -6,17 +6,17 @@ import Post from "../assets/Navicon/pencil.png";
 
 const Home = () => {
   return (
-    <div className="w-auto bg-slate-100 border-gray-300">
+    <div className="w-[650px] bg-slate-100 border-gray-300">
       <div className="h-auto rounded-lg bg-white border-2 ">
         <div className="flex flex-row">
-          <div className="p-4 ">
+          <div className="p-3 ">
             <img
               src={Profile}
               alt=""
               style={{ height: "30px", width: "36px" }}
             />
           </div>
-          <div className="border-2 rounded-full text-md w-full h-auto m-3 bg-gray-100 text-gray-700 hover:bg-zinc-200 transition-all ease-in-out">
+          <div className="border-2 rounded-full text-sm w-full h-auto m-3 bg-gray-100 text-gray-700 hover:bg-zinc-200 transition-all ease-in-out">
             <input
               className=" w-full p-1 ml-2 outline-none bg-transparent cursor-pointer"
               type="text"
@@ -24,32 +24,32 @@ const Home = () => {
             />
           </div>
         </div>
-        <div className="flex flex-row p-1 h-auto ">
+        <div className="flex flex-row p-1 h-auto mt-0">
 
-          <div className="flex center rounded-full text-gray-500 font-semibold hover:bg-zinc-100 cursor-pointer transition-all ease-in-out justify-center w-full px-2 ">
+          <div className="flex center rounded-full text-sm text-gray-500 font-semibold hover:bg-zinc-100 cursor-pointer transition-all ease-in-out justify-center w-full px-2 ">
             
               <img
-                style={{ height: "18px", width: "18px", margin: "5px" }}
+                style={{ height: "16px", width: "16px", margin: "5px" }}
                 src={Question}
                 alt=""
               />
             
             Ask
           </div>
-          <div className="flex rounded-full text-gray-500 font-semibold hover:bg-zinc-100 cursor-pointer transition-all ease-in-out justify-center w-full px-2">
+          <div className="flex rounded-full text-sm text-gray-500 font-semibold hover:bg-zinc-100 cursor-pointer transition-all ease-in-out justify-center w-full px-2">
             
               <img
-                style={{ height: "18px", width: "18px", margin: "5px" }}
+                style={{ height: "16px", width: "16px", margin: "5px" }}
                 src={Answer}
                 alt=""
               />
            
            Answer
           </div>
-          <div className="flex rounded-full text-gray-500 font-semibold hover:bg-zinc-100 cursor-pointer transition-all ease-in-out justify-center w-full px-2">
+          <div className="flex rounded-full text-sm text-gray-500 font-semibold hover:bg-zinc-100 cursor-pointer transition-all ease-in-out justify-center w-full px-2">
             <div className="">
               <img
-                style={{ height: "18px", width: "18px", margin: "5px" }}
+                style={{ height: "16px", width: "16px", margin: "5px" }}
                 src={Post}
                 alt=""
               />
