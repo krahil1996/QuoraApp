936685b4db78afbe1b8f5fe52e38cfb289ad22b9
krahil1996 <rahil393@gmail.com>
2024-09-27 15:18:49 +0530

    wwww

--- myapp/src/components/PostPopup.tsx
+++ myapp/src/components/PostPopup.tsx
@@ -35,7 +35,6 @@ const PostPopup = (props: postType) => {
       aria-modal="true"
     >
       <div className="fixed inset-0 bg-zinc-950 bg-opacity-80 transition-opacity"></div>
-
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <div className="relative transform rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl">
